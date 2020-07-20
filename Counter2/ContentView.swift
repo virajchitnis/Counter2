051d9d7c35d46ad2a1874df0c30ab7e354ba02d2
Viraj Chitnis <chitnisviraj@gmail.com>
2020-07-21 00:32:14 +0100

    Initial Commit

---
+++ Counter2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Counter2
+//
+//  Created by Viraj Chitnis on 7/21/20.
+//  Copyright © 2020 Viraj Chitnis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
