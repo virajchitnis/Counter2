4aa692389eadfafe9d53064c9c2245a134cbe303
Viraj Chitnis <chitnisviraj@gmail.com>
2020-07-21 16:54:50 +0100

    Added CounterItemView design

---
+++ Counter2/CounterItemView.swift
@@ -0,0 +1,68 @@
+//
+//  CounterItemView.swift
+//  Counter2
+//
+//  Created by Viraj Chitnis on 7/21/20.
+//  Copyright © 2020 Viraj Chitnis. All rights reserved.
+//
+
+import SwiftUI
+
+struct CounterItemView: View {
+    var counter: Counter
+    var saveCallBack: () -> Void
+    
+    var title: String {
+        get {
+            if let theTitle = self.counter.title {
+                return theTitle
+            } else {
+                return ""
+            }
+        }
+    }
+    var note: String {
+        get {
+            if let theNote = self.counter.note {
+                return theNote
+            } else {
+                return ""
+            }
+        }
+    }
+    var count: String {
+        get {
+            return "\(self.counter.count)"
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text("\(self.title)")
+                .font(.headline)
+            Text("\(self.note)")
+                .foregroundColor(Color.gray)
+            HStack {
+                Button(action: {}) {
+                    Image(systemName: "minus.circle")
+                }
+                .font(.title)
+                Text("\(self.count)")
+                    .font(.largeTitle)
+                    .fontWeight(.thin)
+                    .padding(.horizontal)
+                Button(action: {}) {
+                    Image(systemName: "plus.circle")
+                }
+                .font(.title)
+            }
+        }
+        .padding(.all)
+    }
+}
+
+struct CounterItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        CounterItemView(counter: Counter(), saveCallBack: {})
+    }
+}
