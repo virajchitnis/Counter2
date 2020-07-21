22f7630356b494cecef83c41d3cb1b95712fc8f7
Viraj Chitnis <chitnisviraj@gmail.com>
2020-07-21 13:23:15 +0100

    Ability to save a counter to CloudKit and fetch a list for display

--- Counter2/ContentView.swift
+++ Counter2/ContentView.swift
@@ -9,8 +9,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @State private var newTitle: String = ""
+    @State private var newNote: String = ""
+    @State private var newCount: String = ""
+    
+    @FetchRequest(
+        entity: Counter.entity(),
+        sortDescriptors: [
+            NSSortDescriptor(keyPath: \Counter.title, ascending: true)
+        ]
+    ) var counters: FetchedResults<Counter>
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            TextField("Title", text: $newTitle)
+                .padding()
+            TextField("Note", text: $newNote)
+                .padding()
+            TextField("Count", text: $newCount)
+                .padding()
+            Button(action: {
+                self.addCounter(title: self.newTitle, note: self.newNote, count: self.newCount)
+            }) {
+                Text("Save")
+            }
+            Divider()
+            List(counters, id: \.self) { counter in
+                Text("\(counter.title ?? "")")
+            }
+        }
+    }
+    
+    func addCounter(title: String, note: String, count: String) {
+        let newCounter = Counter(context: self.managedObjectContext)
+        newCounter.title = title
+        newCounter.note = note
+        if let countInt: Int64 = Int64(count) {
+            newCounter.count = countInt
+        }
+        self.saveContext()
+    }
+    
+    func saveContext() {
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print("Error saving managed object context: \(error)")
+        }
     }
 }
 
